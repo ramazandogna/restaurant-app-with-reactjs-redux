6c04b81086fbd2b8368475ffe8917a9b2c173da6
Ramazan Doğan <doganrmzn40@gmail.com>
2022-06-07 13:09:53 +0300

    Added db.json

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -1,58 +1,66 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { Badge, ListGroup, ListGroupItem } from 'reactstrap';
-import { bindActionCreators } from 'redux';
-import *as categoryActions from '../../redux/actions/categoryActions';
-import *as productActions from '../../redux/actions/productActions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Badge, ListGroup, ListGroupItem } from "reactstrap";
+import { bindActionCreators } from "redux";
+import * as categoryActions from "../../redux/actions/categoryActions";
+import * as productActions from "../../redux/actions/productActions";
 
 class CategoryList extends Component {
+  componentDidMount() {
+    this.props.actions.getCategories();
+  }
 
-    componentDidMount() {
-        this.props.actions.getCategories()
-    }
+  selectCategory = (category) => {
+    this.props.actions.changeCategory(category);
+    this.props.actions.getProducts(category.id);
+  };
 
-    selectCategory = (category) => {
-        this.props.actions.changeCategory(category)
-        this.props.actions.getProducts(category.id)
-    }
-
-    render() {
-        return (
-            <div>
-                <h3><Badge color="warning"> Categories: {this.props.categories.length}</Badge></h3>
-                <ListGroup>
-                    {
-                        this.props.categories.map(category => (
-                            <ListGroupItem
-                                active={category.id === this.props.currentCategory.id}
-                                onClick={() => this.selectCategory(category)}
-                                key={category.id}
-                            >
-                                {category.categoryName}
-                            </ListGroupItem>
-                        ))}
-
-                </ListGroup>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h3>
+          <Badge color="warning">
+            {" "}
+            Categories: {this.props.categories.length}
+          </Badge>
+        </h3>
+        <ListGroup>
+          {this.props.categories.map((category) => (
+            <ListGroupItem
+              active={category.id === this.props.currentCategory.id}
+              onClick={() => this.selectCategory(category)}
+              key={category.id}
+            >
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        currentCategory: state.changeCategoryReducer,
-        categories: state.categoryListReducer
-    }
+  return {
+    currentCategory: state.changeCategoryReducer,
+    categories: state.categoryListReducer,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        actions: {
-            getCategories: bindActionCreators(categoryActions.getCategories, dispatch),
-            changeCategory: bindActionCreators(categoryActions.changeCategory, dispatch),
-            getProducts: bindActionCreators(productActions.getProducts, dispatch)
-        }
-    }
+  return {
+    actions: {
+      getCategories: bindActionCreators(
+        categoryActions.getCategories,
+        dispatch
+      ),
+      changeCategory: bindActionCreators(
+        categoryActions.changeCategory,
+        dispatch
+      ),
+      getProducts: bindActionCreators(productActions.getProducts, dispatch),
+    },
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CategoryList); //fonksiyon dönderiyor, biz de oraya CategoryList'i atıyoruz..
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList); //fonksiyon dönderiyor, biz de oraya CategoryList'i atıyoruz...
